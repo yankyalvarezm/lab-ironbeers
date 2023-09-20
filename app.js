248157fd8fb92d0197db3185bd9830ebaeedfe69
yanky alvarez <yankyalvarez@yankys-iMac.attlocal.net>
2023-09-20 17:43:36 -0400

    lab done

--- app.js
+++ app.js
@@ -22,4 +22,37 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/home', (req, res) => {
+  res.render('home');
+});
+
+app.get('/beers', (req, res) => {
+
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      console.log('beers from api', beersFromApi);
+      res.render('beers', { beersFromApi })
+    })
+
+    .catch(error => console.log(error));
+
+});
+
+app.get('/random-beers', (req, res) => {
+
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      console.log('beers from random:', responseFromAPI);
+
+      res.render('random-beers.hbs', { responseFromAPI })
+
+    })
+    .catch(error => console.log(error));
+
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+console.log('app.js - test')
